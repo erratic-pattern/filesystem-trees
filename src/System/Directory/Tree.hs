c1616d84cb7c2a2a0f9a50689e563dedd28abab5
Adam Curtis <acurtis@spsu.edu>
2012-05-04 15:30:31 -0400

    use lazy IO to avoid time and space leaks; use an opaque Directory type with an IsString instance for directories

--- src/System/Directory/Tree.hs
+++ src/System/Directory/Tree.hs
@@ -1,19 +1,26 @@
-module System.Directory.Tree where
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+module System.Directory.Tree 
+       (Directory, getDir 
+       )where
 
-import System.Directory
-import System.FilePath
-import Data.Tree
+import System.IO.Unsafe (unsafeInterleaveIO)
+import System.Directory (getDirectoryContents, doesDirectoryExist)
+import System.FilePath ((</>))
+import Data.Tree (Tree(..))
+import Data.String (IsString, fromString)
 
 import Control.Monad
 import Control.Applicative
 
-getDirectory :: FilePath -> IO (Tree FilePath)
-getDirectory path = Node path <$> getChildren
+newtype Directory = Directory FilePath deriving (Eq, Ord, Show, Read, IsString)
+
+getDir :: Directory -> IO (Tree FilePath)
+getDir (Directory path) = Node path <$> getChildren
   where getChildren = do
           children <- map (path </>) . filter (`notElem` [".",".."]) 
                       <$> getDirectoryContents path
           forM children $ \c -> do
             p <- doesDirectoryExist c
             if p 
-              then getDirectory c
+              then unsafeInterleaveIO . getDir . fromString $ c
               else return $ Node c []
