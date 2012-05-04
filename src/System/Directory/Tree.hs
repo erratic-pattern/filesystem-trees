b90f87a846722a62f46721fce1650428b08930c0
Adam Curtis <acurtis@spsu.edu>
2012-05-04 15:32:50 -0400

    export getFilePath

--- src/System/Directory/Tree.hs
+++ src/System/Directory/Tree.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module System.Directory.Tree 
-       (Directory, getDir 
+       (Directory, getFilePath, getDir 
        )where
 
 import System.IO.Unsafe (unsafeInterleaveIO)
