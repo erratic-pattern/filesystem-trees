2084067bb69aa4483887f099e23e8aeedcc46b78
Adam Curtis <acurtis@spsu.edu>
2012-05-05 00:11:26 -0400

    remove Directory newtype

--- src/System/Directory/Tree.hs
+++ src/System/Directory/Tree.hs
@@ -1,36 +1,56 @@
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving, RecordWildCards #-}
 module System.Directory.Tree 
-       (Directory, getFilePath, getDir, getDir'
+       (Options(..), defaultOptions,
+        getDir, getDir',
+        getDirectory, getDirectory'
        )where
 
 import System.IO.Unsafe (unsafeInterleaveIO)
 import System.Directory (getDirectoryContents, doesDirectoryExist)
 import System.FilePath ((</>))
+import System.Posix.Files (getSymbolicLinkStatus, isSymbolicLink)
 import Data.Tree (Tree(..))
 import Data.String (IsString, fromString)
+import Data.Default
 
 import Control.Monad
 import Control.Applicative
+import Control.Cond
 
-newtype Directory = Directory FilePath deriving (Eq, Ord, Show, Read, IsString)
 
-getFilePath :: Directory -> FilePath
-getFilePath (Directory p) = p
+data Options = Options { followSymLinks :: Bool } deriving (Eq, Show)
 
-getDir :: Directory -> IO (Tree FilePath)
+instance Default Options where
+  def = Options { followSymLinks = True }
+
+defaultOptions :: Options
+defaultOptions = def
+
+getDirectory :: FilePath -> IO (Tree FilePath)
+getDirectory = getDir defaultOptions
+
+getDirectory' :: FilePath -> IO (Tree FilePath)
+getDirectory' = getDir' defaultOptions
+
+getDir :: Options -> FilePath -> IO (Tree FilePath)
 getDir = _getDir unsafeInterleaveIO
 
-getDir' :: Directory -> IO (Tree FilePath)
+getDir' :: Options -> FilePath -> IO (Tree FilePath)
 getDir' = _getDir id
 
-_getDir :: (IO (Tree FilePath) -> IO (Tree FilePath)) -> Directory
+_getDir :: (IO (Tree FilePath) -> IO (Tree FilePath)) 
+           -> Options
+           -> FilePath
            -> IO (Tree FilePath)
-_getDir f (Directory path) = Node path <$> getChildren
+_getDir f o@Options {..} path = Node path <$> getChildren
   where getChildren = do
           children <- map (path </>) . filter (`notElem` [".",".."]) 
                       <$> getDirectoryContents path
-          forM children $ \c -> do
-            p <- doesDirectoryExist c
-            if p 
-              then f . getDir . fromString $ c
-              else return $ Node c []
+          forM children $ \c ->
+            ifM (doesDirectoryExist c <&&> (return followSymLinks
+                                            <||> (not <$> isSymLink c)))
+              ( f . _getDir f o . fromString $ c )
+              ( return $ Node c [] )
+                       
+isSymLink :: FilePath -> IO Bool
+isSymLink p = isSymbolicLink <$> getSymbolicLinkStatus p
